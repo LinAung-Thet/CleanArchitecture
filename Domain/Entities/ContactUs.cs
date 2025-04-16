da3567c533d83be819edd383f0744c0a63a84101
LinAung-Thet <laungthet@gmail.com>
2025-04-16 19:01:48 +1200

    Domain project
    - Added the ContactUs entity

---
+++ Domain/Entities/ContactUs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities;
+
+public partial class ContactUs
+{
+    [Key]
+    public int ContactUsId { get; set; }
+
+    [StringLength(100)]
+    public string UserName { get; set; } = null!;
+
+    [StringLength(100)]
+    public string UserEmail { get; set; } = null!;
+
+    [StringLength(2000)]
+    public string MessageDetail { get; set; } = null!;
+}
