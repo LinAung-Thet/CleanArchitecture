239d8de2f8291f3f6b9186a5635a22f41ffa0a1e
LinAung-Thet <laungthet@gmail.com>
2025-05-20 23:45:26 +1200

    Domain project
    - Changed the namespace of the entities in the Domain layer.

--- Domain/Entities/Notification.cs
+++ Domain/Entities/Notification.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace LSC.SmartCertify.Domain.Entities;
+namespace Domain.Entities;
 
 [Table("Notification")]
 public partial class Notification
