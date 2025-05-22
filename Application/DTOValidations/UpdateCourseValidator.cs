8cdf9765ff393b89901ce1d1d934ed3ddd8bd00f
LinAung-Thet <laungthet@gmail.com>
2025-05-22 22:14:38 +1200

    Application layer
    - Added validator for the instances of ICourseRepository on updates.

---
+++ Application/DTOValidations/UpdateCourseValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Application.DTOs;
+using Application.Interfaces.Courses;
+
+namespace Application.DTOValidations
+{
+    public class UpdateCourseValidator : AbstractValidator<UpdateCourseDto>
+    {
+        public UpdateCourseValidator(ICourseRepository repository)
+        {
+            RuleFor(x => x.Title).NotNull()
+                .NotEmpty()
+                .MaximumLength(100)
+                .MustAsync(async (title, cancellation) =>
+                    title == null || !await repository.IsTitleDuplicateAsync(title))
+                .WithMessage("The course title must be unique.");
+            RuleFor(x => x.Description)
+                .NotNull()
+                .NotEmpty()
+               .MaximumLength(500);
+        }
+    }
+
+}
